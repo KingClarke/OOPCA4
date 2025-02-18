ee4ef511db9d146f7154e989c5fb7843728a9f88
KingClarke <D00267783@student.dkit.ie>
2025-02-18 11:59:20 +0000

    Forgot to update the github, late push now

--- src/main/java/DAO/DatabaseConnection.java
+++ src/main/java/DAO/DatabaseConnection.java
@@ -3,16 +3,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnetion {
+public class DatabaseConnection {
     private static Connection conn = null;
 
     public static Connection getConnection() {
         if (conn == null) {
             try {
-                // Update the connection URL, username, and password
                 String url = "jdbc:mysql://localhost:3306/expense_record";
-                String username = "root"; // Default username for XAMPP MySQL
-                String password = ""; // Default password for XAMPP MySQL is empty
+                String username = "root";
+                String password = "";
                 conn = DriverManager.getConnection(url, username, password);
             } catch (SQLException e) {
                 e.printStackTrace();
